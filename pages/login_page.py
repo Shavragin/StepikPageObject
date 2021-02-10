9888a827746200fc8f16fb9f18bbb486844e9ad0
Antony S <Disclers2@yandex.ru>
2021-02-10 14:44:09 +0300

    add new login page

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self, browser):
+        # проверка на корректный url адрес
+        url = browser.current_url()
+        assert "login" in url, "This is not login page"
+
+    def should_be_login_form(self):
+        # проверка, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "There is no login form"
+
+    def should_be_register_form(self):
+        # проверка, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "There is no register form"
